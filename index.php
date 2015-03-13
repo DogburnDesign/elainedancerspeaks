30ec387ea41de1185acdcadd20d4acea6d570c42
DJ Wilkerson <djay.wilk@gmail.com>
2015-03-13 17:27:16 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- TITLES, BASE URL AND META DATA -->
+        <title>Elaine Dancer | Dallas, TX Motivational and Public Speaking</title>
+        <base href="http://localhost:8888/elainedancerspeaks/">
+        
+        <!-- STYLING -->
+        <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/edsstyles.css">
+    </head>
+    <body>
+        <video autoplay loop poster="images/itsallaboutlove-bg.jpg" id="bgvid">
+            <source src="images/itsallaboutlove-bg.mp4" type="video/mp4">
+        </video>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="text-align:center;color:#fff;">
+                    <h1>ElaineDancerSpeaks.com<br /> Coming Soon</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <a href="#" class="btn btn-default btn-lg btn-block eds-btn" role="button">Some Button Text Here</a>
+                </div>
+            </div>
+        </div>
+        
+        
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
