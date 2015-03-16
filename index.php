56b086233010ae70a2e2021ef49c8e38aa9c9303
DJ Wilkerson <djay.wilk@gmail.com>
2015-03-16 10:48:06 -0500

    Styling Changes
    Minor Styling changes to the splash page.

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <!-- TITLES, BASE URL AND META DATA -->
         <title>Elaine Dancer | Dallas, TX Motivational and Public Speaking</title>
         <base href="http://localhost:8888/elainedancerspeaks/">
+        <meta name="description" content="Elaine Dancer is a public/motivational speaker available for a range of events in Dallas, TX.">
+        <link rel="author" href="http://dogburndesign.com"/>
         
         <!-- STYLING -->
         <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
@@ -14,15 +16,22 @@
             <source src="images/itsallaboutlove-bg.mp4" type="video/mp4">
         </video>
         
-        <div class="container">
+        <div class="container eds-comingsoon">
             <div class="row">
-                <div class="col-md-12" style="text-align:center;color:#fff;">
-                    <h1>ElaineDancerSpeaks.com<br /> Coming Soon</h1>
+                <div class="col-md-6 col-md-offset-3">
+                    <h1>ElaineDancerSpeaks.com<br /> Coming Soon</h1><hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 col-sm-12 col-xs-12 eds-shortdesc">
+                    <p>Ms. Dancer is a gifted and inspiring speaker on a wide range of spiritual and motivational topics. She will make you cry, but also make you laugh. Her expressive and spontaneous style creates an instant bond with people, as she touches the hearts of everyone she encounters. She has spoken to men and women in correctional facilities, as well as law firms, Sunday school classes, retreats, and professional organizations. Her passion is to encourage and motivate people to believe in themselves.</p>
+                    <p>
+Ms. Dancer recently authored the book "It's All About Love," which she describes as being about "a mother and her gay son's loving journey through the ravages of AIDS to the riches of heaven."</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4 col-md-offset-4">
-                    <a href="#" class="btn btn-default btn-lg btn-block eds-btn" role="button">Some Button Text Here</a>
+                    <a href="http://elainedancer.com" class="btn btn-default btn-lg btn-block eds-btn" role="button">elainedancer.com</a>
                 </div>
             </div>
         </div>
